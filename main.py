80f89e646429e1aac10f0d5ea82d44215ad6bbc7
Francois <119730427+omelette-bio@users.noreply.github.com>
2023-08-10 18:43:54 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
 import uuid
 import json
 import os
-
-# # open json file and load it
-# with open('games.json') as json_file:
-#     data = json.load(json_file)
-
-# #generate guid
-# guid = uuid.uuid4()
-
-# name = input("enter game name : ")
-# path = input("enter game path : ")
-# icon = input("enter game icon path : ")
-
 import sqlite3
 
 json_file = os.open("game.json", os.O_APPEND | os.O_RDWR | os.O_CREAT)
@@ -28,3 +16,41 @@ data = data[0][0].split(b"\n")
 for i in data:
     os.write(json_file, i + b"\n")
 
+os.close(json_file)
+#open json file and load it
+with open('game.json') as json_file:
+    data = json.load(json_file)
+
+#generate guid
+guid = uuid.uuid4()
+
+name = input("enter game name : ")
+path = input("enter game path : ")
+icon = input("enter game icon path : ")
+
+#add info to json into the application section,
+#it must loook like this
+# {
+#     "applicationId": "ecc7d9ab-dfa8-4070-a7d8-8460187b496c",
+#     "applicationPath": "C:\\XboxGames\\The Elder Scrolls V- Skyrim Special Edition (PC)\\Content\\SkyrimSE.exe",
+#     "isCustom": true,
+#     "name": "Skyrim",
+#     "posterPath": "c:\\Users\\omele\\AppData\\Local\\LGHUB\\icon_cache\\53306-1.jpg"
+# },
+
+data["applications"]["applications"].append({"applicationId": str(guid), "applicationPath": path, "isCustom": True, "name": name, "posterPath": icon})
+
+#write the new json to the file
+with open('game.json', 'w') as outfile:
+    json.dump(data, outfile)
+
+#read the json file and write it to the database
+with open('game.json') as json_file:
+    data = json_file.read()
+
+query = "UPDATE data SET file = ?"
+
+cursor.execute(query, (data,))
+
+conn.commit()
+os.remove("game.json")
